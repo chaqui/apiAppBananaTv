678375ed5c09f0bf7c23b7510318a12ca3f0aad9
Josue <josue.chaqui@gmail.com>
2014-05-01 13:37:47 -0600

    agregando la estructura logica de la api y el index

---
+++ api.php
@@ -0,0 +1,37 @@
+<?php
+	include_once('globals.php');
+	include_once('dbmanager.php');
+	/**
+	*/
+	class api 
+	{
+
+		
+		 private function getCanales($idCanal){
+		 	$consulta="SELECT * FROM canal WHERE idcanal =".$idCanal.";";
+			$db = new dbmanager();
+			return $db->executeQuery($consulta);	 	
+		 }
+		 private function getHorariosyCanales($idCanal){
+		 	$canales= $this->getCanales($idCanal);
+		 	$consulta="SELECT * FROM programa WHERE canal_idcanal =".$idCanal.";";
+		 	$db = new dbmanager();
+			$programas= $db->executeQuery($consulta);	
+		 	$json = "";
+		 	$json .="{\"canal:\":";
+		 	while($canal = mysql_fetch_array($canales))
+		 		{
+		 			$json.="{\"nombre\":\"".$canal['nombre']."\",\"programas\":[";
+		 			while($programa = mysql_fetch_array($programas)){
+		 				$json.="{programa:{\"nombre\":\"".$programa['nombre']."\",\"horario\":\"".$programa['horaDeInicio']."\"}}";
+		 				$json.="]}";
+		 				$json.="}";
+		 			}
+		 		}
+		 	return $json;	
+		 }
+		 public function getApi($canal)
+		 {
+		 	return $this->getHorariosyCanales($canal);
+		 }
+	}
